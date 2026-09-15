ab9ea138a2cdfc173551ce30984f7695f586d14e
luizcarlosaguiarprime-dev <luizcarlosaguiarprime@gmail.com>
2026-09-15 19:17:03 -0400

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,11 +1,24 @@
 import os
 import telebot
+from flask import Flask
+from threading import Thread
 
 TOKEN = os.environ["BOT_TOKEN"]
+
 bot = telebot.TeleBot(TOKEN)
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bot funcionando!"
+
+def run():
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
 
 @bot.message_handler(func=lambda message: True)
 def responder(message):
     bot.reply_to(message, "Olá! Seu bot está funcionando! 🤖")
+
+Thread(target=run).start()
 
 bot.infinity_polling()
